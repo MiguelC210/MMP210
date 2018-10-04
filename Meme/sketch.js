425ab070e15292f010cbd62e68bf18bfa662fa6a
Miguel <42749774+MiguelC210@users.noreply.github.com>
2018-10-04 15:18:36 -0400

    update

--- Meme/sketch.js
+++ Meme/sketch.js
@@ -1,14 +1,26 @@
-// Meme version one
+/*Meme version one
+*/
+
+var pika; // global 
+
+function preload(){
+    pika = loadImage("pika.jpg");
+}
 
 function setup() {
     createCanvas(500, 500);
 }
 
 function draw() {
-    background(0);
-    fill("yellow");
+    background(255);  
+    
+    image(pika, 0, 0, width, height);
+    
+    fill("black");
     textAlign(CENTER, CENTER);
-    textSize(100);
+    textSize(25);
     textFont("Comic Sans MS");
-    text("MEME", width/2, height/2);
+    text("When you realize you forgot to do", 250, 15);
+    text("your MMP210 homework", 250, 43);
+    text("and it's due in 20 mins!", 250, 70);
 }
