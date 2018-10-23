7bb01393212662a844238a5cdf5f8636303de325
Miguel <42749774+MiguelC210@users.noreply.github.com>
2018-10-22 22:57:02 -0400

    Final Meme

--- Meme3/sketch.js
+++ Meme3/sketch.js
@@ -7,6 +7,8 @@ var sleep; // global
 var eat; // global
 var r = 0;
 var c = 1; //change
+var m = 0;
+
 
 function preload(){
     panda = loadImage("panda.jpg");
@@ -20,7 +22,12 @@ function setup() {
 }
 
 function draw() {
-    background(255);  
+    background(0);  
+    
+    translate(width/300, height/300);
+    var r = frameCount / 3000 * PI;
+    shearX(r);
+    
     
   if (mouseX < width/2 && mouseY < height/2){
     image(panda, 0,0,width,height);
@@ -29,22 +36,22 @@ function draw() {
    text("When you have to study for", 130, 35);
     textSize(40);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     
-      r += c;
-      if (r > 255 || r < 0){
+      m += c;
+      if (m > 255 || m < 0){
           c *= -1;
       }
    
     text("an exam",150,80);
     textSize(25);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     
     text("Me: ", 50, 150);
     textSize(40);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     } 
     
         else if (mouseX > width/2 && mouseY < height/2) {
@@ -53,21 +60,21 @@ function draw() {
     text("When you finally open your book", 10, 35);
     textSize(40);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
         
-            r += c;
-            if (r > 255 || r < 0){
+            m += c;
+            if (m > 255 || m < 0){
                 c *= -1;
             }
    
     text("to study", 40, 83);
     textSize(25);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     
     text("Also me: ", 50, 150);
     textSize(40);
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     } 
         else if (mouseX < width/2 && mouseY > height/2){
     image(eat,0,0,width,height)
@@ -75,17 +82,17 @@ function draw() {
     text("When the exam is today", 250, 90);
     textSize(40);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     
-            r += c;
-            if (r > 255 || r < 0){
+            m += c;
+            if (m > 255 || m < 0){
                 c *= -1;
             }
     
     text("Also me: ", 230, 250);
     textSize(40);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
         
     
     } 
@@ -95,27 +102,27 @@ function draw() {
     text("When you still have another day to", 10, 25);
     textSize(25);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     
-            r += c;
-            if (r > 255 || r < 0){
+            m += c;
+            if (m > 255 || m < 0){
                 c *= -1;
             }
    
     text("study for the", 5, 80);
     textSize(25);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     
     text("exam", 5, 150);
     textSize(25);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);        
+    fill(m, 0, 100);        
             
     text("Me: ", 390, 120);
     textSize(30);
     textFont("Comic Sans MS");
-    fill(r, 0, 100);
+    fill(m, 0, 100);
     }    
    
 }
